b4f5e76d833ef01e9ed6d8891a7141a7add39fa9
Zakir Bangash <engrzakirbangash@gmail.com>
2024-02-26 21:55:00 +0500

    improve code

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,10 @@ import {configureStore} from '@reduxjs/toolkit';
 import {product} from '../services/products';
 import {setupListeners} from '@reduxjs/toolkit/query';
 import cart from './cart';
-import contact from './contact';
 
 export const store = configureStore({
   reducer: {
     cart: cart,
-    contact: contact,
     [product.reducerPath]: product.reducer,
   },
   middleware: getDefaultMiddleware =>
